c1426bb5a82bd3a0c059617105d711c15e1fbaae
magnus <magnus@dahleide.com>
2024-10-04 18:56:08 +0200

    add no-store to cache object

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,11 @@
 export const dynamic = 'force-dynamic'
 
 export default async function robots() {
-  const time = await fetch("http://worldtimeapi.org/api/timezone/Europe/Paris");
+  const time = await fetch("http://worldtimeapi.org/api/timezone/Europe/Paris",
+    {
+      cache: 'no-store'
+    }
+  );
   const unixtime = await time.json();
   return {
     rules: {
